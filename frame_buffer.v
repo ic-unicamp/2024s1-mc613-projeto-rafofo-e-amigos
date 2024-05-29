a5edb7daa9eae511f135b93225c93441cc1a0e1f
eduardorittner <eduardorittnerc@gmail.com>
2024-05-29 17:38:31 +0000

    a

--- frame_buffer.v
+++ frame_buffer.v
@@ -9,13 +9,25 @@ module frame_buffer #(
   input [9:0] renderer_y,
 
   input vga_clk,
-  input vga_write,
+  input vga_read,
   output [5:0] vga_dado,
   input [9:0] vga_x,
   input [9:0] vga_y
 );
 
-  reg [5:0] map [SCREEN_HEIGHT:0][SCREEN_WIDTH:0];
+  reg [5:0] map [SCREEN_HEIGHT-1:0][SCREEN_WIDTH-1:0];
+
+  always @(posedge renderer_clk) begin
+    if (renderer_write) begin
+      map[renderer_y][renderer_x] <= renderer_dado;
+    end
+  end
+
+  always @(posedge vga_clk) begin
+    if (vga_read) begin
+      vga_dado <= map[vga_y][vga_x];
+    end
+  end
 
 
 endmodule
--- renderer.v
+++ renderer.v
@@ -1,18 +1,92 @@
 module renderer #(
+    parameter BLOCK_SIZE,
     parameter MAPA_WIDTH,
     parameter MAPA_HEIGHT,
     parameter SCREEN_WIDTH,
     parameter SCREEN_HEIGHT,
 ) (
+
+    input clk,
     input vga_active,
-    input [2:0] mapa_block,
+
+    input [3:0] mapa_block,
     output [9:0] mapa_x,
     output [9:0] mapa_y,
     output mapa_read,
+
     output [5:0] buffer_cor,
     output [9:0] buffer_x,
     output [9:0] buffer_y,
     output buffer_write
 );
+
+    reg [3:0] block_aux = 0;
+    reg [7:0] block_x = 0;
+    reg [7:0] block_y = 0;
+    reg done = 0;
+
+    always @(posedge clk) begin
+        if (~vga_active && ~done) begin
+
+            if (block_x == 0 && block_y == 0) begin
+                mapa_read = 1;
+            end else begin
+                mapa_read = 0;
+            end
+
+            if (mapa_block == 4'b0001) begin
+                // Fruta
+                buffer_dado = 6'b110000;
+            end else if (mapa_block == 4'b0010) begin
+                // Obstáculo
+                buffer_dado = 6'b111111;
+            end else if (mapa_block & 4'b1100 == 4'b1000) begin
+                // Cobra 1
+                buffer_dado = 6'b001100;
+            end else if (mapa_block & 4'b1100 == 4'b1100) begin
+                // Cobra 2
+                buffer_dado = 6'b001111;
+            end else begin
+                buffer_dado = 6'b0;
+            end
+
+            buffer_write = 1;
+
+            block_x = block_x + 1;
+
+            if (block_x == BLOCK_SIZE) begin
+                // Linha do bloco
+                block_x = 0;
+                block_y = block_y + 1;
+
+                if (block_y == BLOCK_SIZE) begin
+                    // Final do bloco
+                    block_y = 0;
+                    mapa_x = mapa_x + 1;
+
+                    if (mapa_x == MAPA_WIDTH) begin
+                        // Linha do mapa
+                        mapa_x = 0;
+                        mapa_y = mapa_y + 1;
+
+                        if (mapa_y == MAPA_HEIGHT) begin
+                            // Final do mapa
+                            mapa_y = 0;
+                            done = 1;
+                        end
+                    end
+                end
+            end
+
+        end else begin
+            mapa_read = 0;
+            mapa_x = 0;
+            mapa_y = 0;
+            buffer_write = 0;
+            buffer_x = 0;
+            buffer_y = 0;
+            done = 0;
+        end
+    end
 
 endmodule
