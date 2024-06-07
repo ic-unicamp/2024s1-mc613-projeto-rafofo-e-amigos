82b7c37f2196204dbf5a0709b6672aa77d1f112c
RafaelCasonato <rafa.casonato@gmail.com>
2024-06-07 18:27:18 -0300

    07/06 - 18:27

--- update.v
+++ update.v
@@ -39,6 +39,7 @@ module update #(
     parameter APAGA_CAUDA = 5;
     parameter ATUALIZA_CAUDA = 6;
     parameter GAME_OVER = 7;
+    parameter INICIAL = 8;
 
     reg [2:0] state = IDLE;
     reg [3:0] aux;
@@ -48,10 +49,12 @@ module update #(
 
     reg [1:0] cauda_dir;
 	
-    reg [9:0] cauda_x = 3;
-    reg [9:0] cauda_y = 3;
+    reg [9:0] cauda_x;
+    reg [9:0] cauda_y;
     reg [9:0] cauda_last_x;
     reg [9:0] cauda_last_y;
+
+    reg inicial = 0;
 
     reg game_over = 0;
     reg comeu_fruta = 0;
@@ -69,6 +72,11 @@ always @(posedge clk) begin
         end
         CALCULA_POSICAO: begin
             // Calcula a proxima posição da cabeça com base na direção
+            if (inicial == 0) begin
+                cauda_x = cabeca_x;
+                cauda_y = cabeca_y;
+                state = INICIAL;
+            end
             case (cobra_dir) 
                 0: begin
                     cabeca_y = (cabeca_y == 0) ? MAPA_HEIGHT - 1 : cabeca_y - 1;
@@ -83,9 +91,8 @@ always @(posedge clk) begin
                     cabeca_x = (cabeca_x == MAPA_WIDTH - 1) ? 0 : cabeca_x + 1;
                 end
             endcase
-
             // Pede a leitura da nova posição pra conferir se ela
-            // é uma fruta ou obstaculo, mais ou menos assim:
+            // é uma fruta ou obstaculo:
             update_rx = cabeca_x;
             update_ry = cabeca_y;
             update_renable = 1;
@@ -93,22 +100,29 @@ always @(posedge clk) begin
         end
         CHECA_COLISAO: begin
             aux = update_rdata;
-            if ((aux[3] == 1 )|| (aux == 4'b0001))begin
+            if ((aux[3] == 1 ) || (aux == 4'b0001))begin
+            // Colidiu com o obstáculo ou com si mesma
                 game_over = 1;
             end else if (aux == 4'b0010) begin
+            // Comeu a fruta    
                 comeu_fruta = 1;
+                tamanho = tamanho + 1; 
             end 
-            // Checa se é uma fruta ou obstaculo ou cobra
-            // Se for um obstaculo: game_over
             state = ESCREVE_CABECA;
         end
         ESCREVE_CABECA: begin
-            // Escreve a cabeça
             update_wx = cabeca_x;
             update_wy = cabeca_y;
             update_wdata = 8 + cobra_dir;
             update_wenable = 1;
             state = CHECA_CAUDA;
+        end
+        INICIAL: begin
+            update_wx = cauda_x;
+            update_wy = cauda_y;
+            update_wdata = 4'b0000;
+            inicial = 1;
+            state = CALCULA_POSICAO;
         end
         CHECA_CAUDA: begin
             // Se não tiver comido uma fruta, pede
@@ -119,7 +133,9 @@ always @(posedge clk) begin
                 update_ry = cauda_y;
                 update_renable = 1;
                 state = APAGA_CAUDA;
-            end 
+            end else if (comeu_fruta == 1) begin
+            // Comeu a fruta e aumenta o tamanho da cobra
+            end
             
         end
         APAGA_CAUDA: begin
